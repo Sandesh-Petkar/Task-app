ea4226a6635ac892d2248f26a697d48fec710037
Sandesh Petkar <sandeshpetkar9@gmail.com>
2020-06-29 13:31:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Fragment } from "react";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.css";
+
+const App = () => {
+  const [inputFields, setInputFields] = useState([
+    { Name: '', email: '' }
+  ]);
+
+  const handleAddFields = () => {
+    const values = [...inputFields];
+      console.log('values',values)
+      
+    values.push({ Name: '', email: '' });
+    console.log('values2',values)
+    setInputFields(values);
+    console.log('final',values)
+  };
+
+  const handleRemoveFields = index => {
+    const values = [...inputFields]; 
+    console.log('val',values) 
+    values.splice(index, 1);
+    setInputFields(values);
+  };
+
+  const handleInputChange = (i, e) => {
+    const values = [...inputFields];
+    if (e.target.name === "Name") {
+      values[i].Name = e.target.value;
+    } else {
+      values[i].email = e.target.value;
+    }
+
+    setInputFields(values);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log("inputFields", inputFields);
+  };
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <h1>Generating input fields</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-row">
+          {inputFields.map((inputField, index) => (
+            <Fragment key={`${inputField}~${index}`}>
+              <div className="form-group col-sm-4">
+                <label htmlFor="Name"> Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="Name"
+                  name="Name"
+                  value={inputField.Name}
+                  onChange={e => handleInputChange(index, e)}
+                />
+              </div>
+              <div className="form-group col-sm-4">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="text" 
+                  className="form-control" 
+                  id="email"
+                  name="email"
+                  value={inputField.email}
+                  onChange={event => handleInputChange(index, event)}
+                />
+              </div>
+              <div className="form-group col-sm-2">
+                
+                <button
+                  className="btn btn-primary"
+                  type="button"
+                  onClick={() => handleAddFields()}
+                >
+                  +
+                </button>
 
-export default App;
+                <button
+                  className="btn btn-danger"
+                  type="button"
+                  onClick={() => handleRemoveFields(index)}
+                >
+                  -
+                </button> 
+
+              </div>
+            </Fragment>
+          ))}
+        </div>
+        <div className="submit-button">
+          <button
+            className="btn btn-primary mr-2"
+            type="submit"
+            onSubmit={handleSubmit}
+          >
+            Save
+          </button>
+        </div>
+        <br/>
+        <pre>
+          {JSON.stringify(inputFields, null, 2)}
+        </pre>
+      </form>
+    </>
+  );
+};
+
+export default App
